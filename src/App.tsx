150ad12fd31ac5315ea8cb65edc0fbff75f39745
gcsandesh <gcsandesh01@gmail.com>
2025-07-20 14:09:24 +0545

    feat: setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <h1 className="font-bold text-xl">Welcome</h1>
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className="p-4 flex flex-col items-center">
+      <h1 className="font-bold text-xl my-4">Welcome to Encyclomedia</h1>
+      <Button variant="secondary" color="green">
+        Sign up
+      </Button>
+    </div>
+  )
 }
 
 export default App
