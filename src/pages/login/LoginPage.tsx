256082c928f32c2a3a376e49ad03d3274274793a
gcsandesh <gcsandesh01@gmail.com>
2025-07-23 10:46:54 +0545

    feat: add some shadcn components

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,6 +1,14 @@
 import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 import { useAuth } from '@/contexts/AuthContext'
 import { loginUser } from '@/services/auth'
+import googleG from '@/assets/google-g.png'
 
 const LoginPage = () => {
   const {} = useAuth()
@@ -10,8 +18,27 @@ const LoginPage = () => {
 
   return (
     <div>
-      <h1 className="font-bold text-xl my-4">Welcome to Encyclomedia</h1>
-      <Button onClick={handleSignIn}>Sign in with Google</Button>
+      <h1 className="font-bold text-5xl my-4 tracking-wide text-center">
+        Encyclomedia🍿
+      </h1>
+
+      <p>
+        Get information on movies, save to watchlist, or keep track of watched
+        movies or shows.
+      </p>
+
+      <Card className="mt-4">
+        <CardHeader className="flex flex-col items-center">
+          <CardTitle className="uppercase">Sign in</CardTitle>
+          <CardDescription>Lets get you signed in!</CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center">
+          <Button onClick={handleSignIn} className="my-4" variant="outline">
+            <img src={googleG} width={27} />
+            Sign in with Google
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   )
 }
